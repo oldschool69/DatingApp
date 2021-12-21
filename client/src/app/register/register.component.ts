2bdd2c0474accfc1cd21ecd729dd6383592e95aa
Flavio Oliveira <flavio.marcondes@gmail.com>
2021-12-21 09:50:34 -0300

    partial implementation

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -14,9 +15,10 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   registerForm: FormGroup;
   maxDate: Date;
+  validationErrors: string[]
 
   constructor(private accountService: AccountService, private toastr: ToastrService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -49,14 +51,12 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    console.log(this.registerForm.value);
-    // this.accountService.register(this.model).subscribe(response => {
-    //  console.log(response);
-    //  this.cancel(); 
-    // }, error => {
-    //   console.log(error);
-    //   this.toastr.error(error.error);
-    // });
+    this.accountService.register(this.model).subscribe(response => {
+      this.router.navigateByUrl('/members');
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
+    });
   }
 
   cancel() {
